3e013826bb2c436eef4719a9c03e731ce5a34e69
David Moncayo <david@stablesoftworks.com>
2021-10-16 16:07:32 -0400

    serve static files

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 5000
 const { default: axios } = require('axios');
   
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/coin_data', (req, res) => {
     axios.get(`https://api.coingecko.com/api/v3/coins/${req.query.coin}/`)
@@ -16,6 +18,10 @@ app.get('/coin_history', (req, res) => {
     .then(query => res.send(query.data))
     .catch(err => res.send(err))
 })
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server running at port ${port}`)
